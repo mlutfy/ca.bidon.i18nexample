dfff1fcc2d50a8bbd62b069555d5ee23b1e64f6e
Mathieu Lutfy <mathieu@bidon.ca>
2014-05-02 20:52:21 -0400

    i18nexample.js: fix the 'domain' argument of ts() strings.

--- i18nexample.js
+++ i18nexample.js
@@ -3,7 +3,7 @@
  */
 cj(function($) {
   $('.crm-i18nexample-test').click(function() {
-    CRM.alert(ts('This is a string in a javascript function in the i18nexample extension. Page generated at %1.', {1: CRM.i18nexample.currentTime, domain: 'i18nexample'}), ts('Yay!', {domain: 'i18nexample'}), 'success');
+    CRM.alert(ts('This is a string in a javascript function in the i18nexample extension. Page generated at %1.', {1: CRM.i18nexample.currentTime, domain: 'ca.bidon.i18nexample'}), ts('Yay!', {domain: 'ca.bidon.i18nexample'}), 'success');
     return false;
   });
 });
